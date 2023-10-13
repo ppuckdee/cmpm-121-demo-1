c6101cc395276cb2a3dc08ee546e1448279afa1f
Nicholas Puckdee <132693105+ppuckdee@users.noreply.github.com>
2023-10-13 16:36:25 -0700

    Step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -15,17 +15,30 @@ button.innerHTML = "🍰";
 app.append(button);
 
 let counter: number = 0;
+let lastMillis = 0;
+let fps = 60; 
 
 const counterDisplay = document.createElement("div");
 counterDisplay.innerHTML = `${counter} cake slices`;
 app.append(counterDisplay);
 
 button.addEventListener("click", () => {
-  counter++;
-  counterDisplay.innerHTML = `${counter} cake slices`;
+    counter += 1 / fps; 
+    counterDisplay.innerHTML = `${counter.toFixed(2)} cake slices`; 
 });
 
-setInterval(() => {
-  counter++;
-  counterDisplay.innerHTML = `${counter} cake slices`;
-}, 1000);
+function tick(millis: number) {
+    const delta = millis - lastMillis;
+    lastMillis = millis;
+
+    if (delta > 0) {
+        fps = Math.round(1000 / delta); 
+        console.log(`FPS: ${fps}`);
+    }
+
+    requestAnimationFrame(tick);
+}
+
+requestAnimationFrame(tick);
+
+
