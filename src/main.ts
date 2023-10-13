e3d8556e0d2817a78ccb5125a1f21bb1d214c5c8
Nicholas Puckdee <132693105+ppuckdee@users.noreply.github.com>
2023-10-12 21:22:54 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Nick's game";
+const gameName = "Nick's cake game";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = "🍰";
+app.append(button);
